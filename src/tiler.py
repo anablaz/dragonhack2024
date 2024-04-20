979a1393bd9b238852d19d2a1e1bc12897ac6dc6
Blaz Rolih <blaz.rolih@gmail.com>
2024-04-20 19:33:55 +0200

    Refactor tiler

--- src/tiler.py
+++ src/tiler.py
@@ -21,36 +21,53 @@ class Tiler:
 
         self.tile_size = tile_size
 
-        self.resizer = Resize(size=in_size)
         self.unfolder = nn.Unfold(kernel_size=tile_size, stride=tile_size)
+        self.folder = torch.nn.Fold(output_size=in_size, kernel_size=tile_size, stride=tile_size)
 
-    def __call__(self, imgs: torch.Tensor):
+    def tile(self, img: torch.Tensor):
         """
         Tile the passed images into specified tiles.
 
         Args:
-            imgs (torch.Tensor): input images of shape [B, C, H, W]
+            img (torch.Tensor): input images of shape [C, H, W]
 
         Returns:
-            tiled image in shape: [b, t_dim_h, t_dim_w, C, t_h, t_w]
+            tiled image in shape: [t_dim_h * t_dim_w, C, t_h, t_w]
         """
-        assert len(imgs.shape) == 4, "Tiler only supports batched input"
-
-        b = imgs.shape[0]
+        assert len(img.shape) == 3, "Tiler only supports non-batched input"
 
         # tile height, tile width
         t_h, t_w = self.tile_size
         # vertical tile count, tile count horizontal
         t_c_h, t_c_w = self.tile_count
 
-        img = self.resizer(imgs)
-
-        # [b, C * t_h * t_w, total_tile_count]
+        # [C * t_h * t_w, total_tile_count]
         tiles = self.unfolder(img)
-        # [b, C, t_h, t_w, total_tile_count] -> [b, total_tile_count, C, t_h, t_w] -> [b, t_dim_h, t_dim_w, C, t_h, t_w]
-        tiles = tiles.view(b, -1, t_h, t_w, t_c_h * t_c_w).permute(0, 4, 1, 2, 3).view(b, t_c_h, t_c_w, -1, t_h, t_w)
+        # [C, t_h, t_w, total_tile_count] -> [total_tile_count, C, t_h, t_w]
+        tiles = tiles.view(-1, t_h, t_w, t_c_h * t_c_w).permute(3, 0, 1, 2)
 
         return tiles
+
+    def untile(self, tiles: torch.Tensor):
+        """
+        Assemble the tiles back into full image.
+
+        Args:
+            tiles (torch.Tensor): tiles of shape [t_dim_h * t_dim_w, C, t_h, t_w]
+
+        Returns:
+            reassembled image in shape: [C, H, W]
+        """
+        # tile height, tile width
+        t_h, t_w = self.tile_size
+        # vertical tile count, tile count horizontal
+        t_c_h, t_c_w = self.tile_count
+
+        # [total_tile_count, C, t_h, t_w] -> [C, t_h, t_w, total_tile_count]
+        tiles = tiles.permute(1, 2, 3, 0).view(-1, t_c_h * t_c_w)
+        untiled = self.folder(tiles)
+
+        return untiled
 
 
 if __name__ == '__main__':
@@ -59,17 +76,24 @@ if __name__ == '__main__':
 
     img = torchvision.io.read_image("../data/test_img.png",
                                     mode=torchvision.io.ImageReadMode.RGB).to(torch.float32)
-    imgs = torch.stack([img, img])
+
+    resize = Resize((1024, 768))
+    img = resize(img)
 
     tiler = Tiler(in_size=(1024, 768), tile_size=(256, 256))
 
-    tiles = tiler(imgs)
-    print(tiles.shape)
+    tiles = tiler.tile(img)
+    untiled = tiler.untile(tiles)
+
+    assert (img == untiled).all()
 
     fig, axs = plt.subplots(4, 3)
     for i in range(4):
         for j in range(3):
-            axs[i][j].imshow(tiles[0][i][j].permute(1, 2, 0).to(torch.uint8))
+            axs[i][j].imshow(tiles[i*3 + j].permute(1, 2, 0).to(torch.uint8))
 
     plt.tight_layout()
+    plt.show()
+
+    plt.imshow(untiled.permute(1, 2, 0).to(torch.uint8))
     plt.show()
